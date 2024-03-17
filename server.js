f76884fa152c26290dae009edd8b12f93fe11375
Rayan <rayan.frem@gmail.com>
2024-03-17 13:53:39 +0400

    Adding ejs and rendering HTML while passing variables from server.js to the index.ejs

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine','ejs')
+
+app.get("/",(req,resp) => {
+   resp.render("index",{text: "test var"})
+})
+
 app.listen(4000)
